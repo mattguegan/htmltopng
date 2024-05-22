314b328e54df7c5db153437da416b1eccb0a7a2e
mattguegan <141003676+mattguegan@users.noreply.github.com>
2024-05-22 11:59:03 +0200

    Update imageGenerator.js

--- imageGenerator.js
+++ imageGenerator.js
@@ -28,43 +28,4 @@ function dataURLToBlob(dataURL) {
   const ab = new ArrayBuffer(byteString.length);
   const ia = new Uint8Array(ab);
 
-  for (let i = 0; i < byteString.length; i++) {
-    ia[i] = byteString.charCodeAt(i);
-  }
-
-  return new Blob([ab], { type: mimeString });
-}
-
-function uploadImageToBubble(dataURL) {
-  const apiUrl = 'https://comparateur-82079.bubbleapps.io/version-test/api/1.1/wf/save_image'; // URL de votre workflow API
-  const blob = dataURLToBlob(dataURL);
-  const formData = new FormData();
-  formData.append('file', blob, 'image.png');
-
-  fetch(apiUrl, {
-    method: 'POST',
-    body: formData
-  })
-    .then(response => {
-      if (!response.ok) {
-        throw new Error('Network response was not ok ' + response.statusText);
-      }
-      return response.json();
-    })
-    .then(data => {
-      console.log('Image uploaded successfully:', data);
-      alert('Image uploaded successfully: ' + JSON.stringify(data));
-    })
-    .catch(error => {
-      console.error('Error uploading image:', error);
-      alert('Error uploading image: ' + error.message);
-    });
-}
-
-function generateAndUploadPNG() {
-  const imgGen = new ImageGenerator();
-  imgGen.generatePNG('group-to-export', uploadImageToBubble);
-}
-
-window.generateAndUploadPNG = generateAndUploadPNG;
-
+  for (let i = 0; i < byteString.length; i
